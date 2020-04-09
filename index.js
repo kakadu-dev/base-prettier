c745cbcefe51b1242f2995feb09837bd51c1a299
Mikhail Yarmaliuk <hellboymxb@gmail.com>
2020-04-09 11:23:29 +0300

    Init

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+	bracketSpacing:     false,
+	jsxBracketSameLine: true,
+	printWidth:         120,
+	useTabs:            true,
+	tabWidth:           4,
+	trailingComma:      'es5',
+	semi:               true,
+	singleQuote:        true,
+	arrowParens:        'avoid',
+};
